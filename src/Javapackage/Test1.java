e8244a882a8470148fb61facf11d1235e15a54e6
Harshada <hingeh9@gmail.com>
2025-07-31 15:55:54 +0530

    Initial commit - Selenium project setup

---
+++ src/Javapackage/Test1.java
@@ -0,0 +1,15 @@
+package Javapackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+	  public static void main(String[] args) {
+	        System.setProperty("webdriver.chrome.driver", "D:\\\\Selenium\\\\chromedriver-win64\\\\chromedriver-win64\\\\chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://www.google.com");
+	        System.out.println("Title: " + driver.getTitle());
+	        driver.quit();
+	    }
+	}
+
